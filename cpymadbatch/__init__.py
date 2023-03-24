cbef78cd69a7048586ddcb904136b94d6c1ac598
ab-gh <git@ash.pm>
2023-03-24 10:13:06 +0100

    test

--- cpymadbatch/__init__.py
+++ cpymadbatch/__init__.py
@@ -1,19 +1,23 @@
 __version__ = '0.1.0'
 
+import itertools
 import os
 
-from typing import Callable
+from typing import Callable, Union, Optional
+
+import numpy
+from cpymad.madx import Madx
 
 class Config:
     """
     Configuration class for a cpymad batch job
     """
 
-    def __init__(self, 
+    def __init__(self,
                  name: str,
-                 madx_executable="/afs/cern.ch/user/m/mad/bin/madx", 
-                 output_dir=None,
-                 local = False
+                 madx_executable="/afs/cern.ch/user/m/mad/bin/madx",
+                 directory=None,
+                 local=False
                  ):
         """
         Initialise a configuration for a cpymad batch job.
@@ -21,7 +25,7 @@ class Config:
             Parameters:
                 name (str): Name of the job
                 madx_executable (str): Path to a MAD-X executable (default: /afs/cern.ch/user/m/mad/bin/madx)
-                output_dir (str): Path to the output directory (default: /eos/user/<u>/<username>/cpymadbatch/<name>)
+                dir (str): Path to the job directory (default: /eos/user/<u>/<username>/cpymadbatch/<name>)
                 local (bool): If True, the job will be run locally, otherwise, it is submitted to HTCondor (default: False)
 
         """
@@ -29,35 +33,54 @@ class Config:
         self.name = name
         self.user = os.environ["USER"]
         i = 0
-        while True:
-            if os.path.exists(f"/eos/user/{self.user[0]}/{self.user}/cpymadbatch/{name}"):
-                i = i + 1
-            else:
-                self.output_dir = output_dir if output_dir else f"/eos/user/{self.user[0]}/{self.user}/cpymadbatch/{self.name + str(i)}/output"
-                break
+        if dir:
+            while True:
+                if os.path.exists(f"/eos/user/{self.user[0]}/{self.user}/cpymadbatch/{name}"):
+                    i = i + 1
+                else:
+                    self.directory = f"/eos/user/{self.user[0]}/{self.user}/cpymadbatch/{self.name + str(i)}/output"
+                    break
+        else:
+            self.directory = directory
         self.local = local
+
 
 class Parameters:
     """
     A set of parameters for a simulation job
     """
 
-    def __init__(self, 
+    def __init__(self,
                  params: dict):
         for key, value in params.items():
             setattr(self, key, value)
 
-        
+    def __iter__(self):
+        for key, value in self.__dict__.items():
+            yield key, value
+
 class Job:
     """
     A single simulation job
     """
 
-    def __init__(self, 
+    def __init__(self,
                  config: Config,
                  function: Callable,
                  parameters: Parameters):
         self.template_function = function
         self.parameters = parameters
+        self.config = config
 
+    def generate(self,
+                 directory: str = None):
+        """
+        Generate the batch jobs
+        """
+        if directory is None:
+            self.batch_directory = self.config.directory+"/batch"
+        else:
+            self.batch_directory = directory
 
+        print(self.parameters.keys())
+        print(type(self.parameters))
--- tests/test_cpymadbatch.py
+++ tests/test_cpymadbatch.py
@@ -1,6 +1,18 @@
-from cpymadbatch import __version__
+import numpy
+import sys
 
+sys.path.append('../cpymadbatch')
+
+import cpymadbatch as cmb
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert cmb.__version__ == '0.1.0'
 
+
+config = cmb.Config("test")
+parameters = cmb.Parameters({"test": [1, 2, 3], "foo": [4, 5, 6], "bar": [10]})
+def test_function(a):
+    return a
+job = cmb.Job(config=config, parameters=parameters, function=test_function)
+
+job.generate()
